81ef6f3f0bd7403a8c7a55e2ce7e31edee7b1653
sanjar-x <sanjar68x@gmail.com>
2024-05-30 22:36:03 +0500

    Allow all origins

--- main.py
+++ main.py
@@ -43,15 +43,12 @@ app = FastAPI(
 )
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:3000",
-        "http://localhost:3000/",
-        "https://quduqloyiha.uz",
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 
 # app.mount("/static", StaticFiles(directory="app/templates/static"), name="static")
 
