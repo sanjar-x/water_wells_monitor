e568b4dd5b6560d7c678fd1080a5fd71fd98031c
sanjar-x <sanjar68x@gmail.com>
2024-05-31 11:01:07 +0500

    Fix statistics endpoint for single well

--- app/routers/endpoints/statistics/get_well_messages.py
+++ app/routers/endpoints/statistics/get_well_messages.py
@@ -3,6 +3,7 @@ from app.services.statistics.get_statistics import get_well_messages
 
 router = APIRouter()
 
+
 @router.get("/statistics/{number}", status_code=status.HTTP_200_OK)
 async def well_statistiks(number: str):
     messages_data = await get_well_messages(number)
--- app/services/statistics/get_statistics.py
+++ app/services/statistics/get_statistics.py
@@ -6,37 +6,13 @@ from sqlalchemy.future import select
 from app.core.database import get_session
 
 
-# async def get_well_messages_unix(number: str, interval: str) -> List[MessageModel]:
-#     current_time = datetime.utcnow()
-#     if interval == "week":
-#         start_time = current_time - timedelta(weeks=1)
-#     elif interval == "month":
-#         start_time = current_time - timedelta(days=30)  # примерно 1 месяц
-#     elif interval == "year":
-#         start_time = current_time - timedelta(days=365)  # 1 год
-#     else:
-#         raise ValueError("Invalid interval specified")
-
-#     async with get_session() as session:
-#         result = await session.execute(
-#             select(MessageModel).where(
-#                 and_(
-#                     MessageModel.number == number,
-#                     MessageModel.time >= start_time
-#                 )
-#             )
-#         )  # type: ignore
-#         messages = result.scalars().all()
-#         return messages
-
-
 async def get_well_messages(number: str) -> List[MessageModel]:
     async with get_session() as session:
         result = await session.execute(
             select(MessageModel).where(MessageModel.number == number)
         )  # type: ignore
         messages = result.scalars().all()
-        return messages
+        return list(messages)
 
 
 async def get_well_messages_within_days(number: str, days: int) -> List[MessageModel]:
@@ -45,9 +21,9 @@ async def get_well_messages_within_days(number: str, days: int) -> List[MessageM
         result = await session.execute(
             select(MessageModel)
             .where(MessageModel.number == number)
-            .where(MessageModel.time >= date_limit.isoformat())
+            .where(MessageModel.received_at >= date_limit.isoformat())
         )  # type: ignore
-        return result.scalars().all()
+        return list(result.scalars().all())
 
 
 async def get_well_messages_last_week(number: str) -> List[MessageModel]:
