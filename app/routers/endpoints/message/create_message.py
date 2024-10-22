6fd09c768c4bf9c3d2db538bee8530802427734a
sanjar X <sanjar68x@gmail.com>
2024-10-22 10:10:19 +0500

    Update create_message.py

--- app/routers/endpoints/message/create_message.py
+++ app/routers/endpoints/message/create_message.py
@@ -9,13 +9,12 @@ from app.schemas.message_schemas import Message
 router = APIRouter()
 
 
+
 @router.post("/message", status_code=status.HTTP_201_CREATED)
 async def create_message_(message_data: Message):
     well = await get_well_by_number(message_data.number[:-1])
     if not well:
         return None
-
-    message_data.temperature = message_data.temperature[2:-1]
 
     message_data.water_level = str(
         int(str(well.depth)) - round(float(message_data.water_level[2:-1]))
@@ -29,7 +28,24 @@ async def create_message_(message_data: Message):
                 float(str(well.salinity_start)), float(str(well.salinity_end))
             )
         )
-
+    if not well.temperature_start:  # type: ignore
+        message_data.temperature = message_data.temperature[2:-1]
+    else:
+        message_data.temperature = str(
+            random.uniform(
+                float(str(well.temperature_start)), float(str(well.temperature_end))
+            )
+        )
+    if not well.water_level_start:  # type: ignore
+        message_data.water_level = str(
+            int(str(well.depth)) - round(float(message_data.water_level[2:-1]))
+        )
+    else:
+        message_data.water_level = str(
+            random.randint(
+                int(str(well.water_level_start)), int(str(well.water_level_end))
+            )
+        )
     await create_message(message_data)
     await generate_well_message()
     return JSONResponse(
