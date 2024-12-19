ad39fc020891f563c8516b1fc942d30f6c232aad
ocean <sanjar-x>
2024-12-19 12:57:02 +0500

    Fix update

--- app/routers/endpoints/well/update_well.py
+++ app/routers/endpoints/well/update_well.py
@@ -6,21 +6,21 @@ from app.schemas.wells_schemas import WelleUpdateSchema, WelleUpdateDevSchema
 router = APIRouter()
 
 
-@router.patch("/wells/{well_id}")
-async def update_well_endpoint(well_id: str, update_well_data: WelleUpdateSchema):
-    existing_well = await get_well(well_id)
-    if not existing_well:
-        return JSONResponse(
-            content={
-                "message": f"Yangilash bekor qilindi! ID: {well_id} quduq ro'yhatdan o'tkazilmagani"
-            },
-            status_code=status.HTTP_404_NOT_FOUND,
-        )
-    await update_well(well_id, update_well_data)
-    return JSONResponse(
-        content={"message": f"Quduq ma'lumotlari yangilandi!"},
-        status_code=status.HTTP_200_OK,
-    )
+# @router.patch("/wells/{well_id}")
+# async def update_well_endpoint(well_id: str, update_well_data: WelleUpdateSchema):
+#     existing_well = await get_well(well_id)
+#     if not existing_well:
+#         return JSONResponse(
+#             content={
+#                 "message": f"Yangilash bekor qilindi! ID: {well_id} quduq ro'yhatdan o'tkazilmagani"
+#             },
+#             status_code=status.HTTP_404_NOT_FOUND,
+#         )
+#     await update_well(well_id, update_well_data)
+#     return JSONResponse(
+#         content={"message": f"Quduq ma'lumotlari yangilandi!"},
+#         status_code=status.HTTP_200_OK,
+#     )
 
 
 @router.patch("/wells/{well_id}")
