2c8125d39c0ff157f195c66e5b77e32dd103451d
sanjar X <sanjar68x@gmail.com>
2025-04-10 00:23:55 +0500

    Update centre.py

--- app/services/centre.py
+++ app/services/centre.py
@@ -8,7 +8,7 @@ from requests import post
 
 async def send_data(code: int, level: float, meneral: float, temperature: float):
     now = datetime.today() + timedelta(hours=5)
-    today = int(now.strftime("%Y%m%d"))
+    today = int(now.strftime("%Y%m%d%H%M"))
     post(
         url="http://89.236.195.198:3010",
         json={
