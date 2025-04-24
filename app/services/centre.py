06b4b2e0ec73d6b393c1c6c68f064e7b941ccdfd
sanjar X <sanjar68x@gmail.com>
2025-04-24 17:39:15 +0500

    Update centre.py

--- app/services/centre.py
+++ app/services/centre.py
@@ -1,14 +1,26 @@
-data = {
-    "code": 860389053632341,
-    "data": {"level": 554.67, "meneral": 1.456, "temperatyra": 14.5, "vaqt": 20250113},
-}
 from datetime import datetime, timedelta
 from requests import post
 
 
 async def send_data(code: int, level: float, meneral: float, temperature: float):
     now = datetime.today() + timedelta(hours=5)
-    today = int(now.strftime("%Y%m%d%H%M"))
+
+    # Получаем текущие часы и минуты
+    current_hour = now.hour
+    current_minute = now.minute
+
+    # Генерируем случайные часы и минуты в пределах текущего времени
+    rand_hour = random.randint(0, current_hour)
+    if rand_hour == current_hour:
+        rand_minute = random.randint(0, current_minute)
+    else:
+        rand_minute = random.randint(0, 59)
+
+    # Формируем случайное время
+    rand_time = now.replace(hour=rand_hour, minute=rand_minute, second=0, microsecond=0)
+
+    today = int(rand_time.strftime("%Y%m%d%H%M"))
+
     post(
         url="http://89.236.195.198:3010",
         json={
